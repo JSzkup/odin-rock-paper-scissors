b1df6d31bb1ea8d95db89f1d5df2acd7ee931334
Jonathan Szkup <jszkup101@gmail.com>
2023-09-14 16:51:24 -0400

    Connect js to HTML, write skeleton functions with comments

---
+++ main.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    // Randomly returns either Rock, Paper, or Scissors
+}
+
+function getPlayerChoice() {
+    // Prompt the player for a choice between Rock, Paper, or Scissors
+}
+
+function playRound(playerSelection, computerSelection) {
+    // plays a SINGLE round of RPS, returns a string that declares the winner
+}
+
+function game() {
+    // Plays 5 rounds in a loop keeping score, then prints the results into console
+}
+
