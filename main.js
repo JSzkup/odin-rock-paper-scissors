32b57460b3a01d0c24e65cc7071e170087a40803
Jonathan Szkup <jszkup101@gmail.com>
2023-09-14 17:13:32 -0400

    Add computer move choices

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 function getComputerChoice() {
     // Randomly returns either Rock, Paper, or Scissors
+    let randNum = Math.floor(Math.random() * 100);
+    let move;
+
+    if (randNum >= 0 && randNum <= 33) {
+        move = "rock"
+    } else if (randNum >= 34 && randNum <= 66) {
+        move = "paper"
+    } else {
+        move = "scissors"
+    }
+
+    return move;
 }
 
 function getPlayerChoice() {
     // Prompt the player for a choice between Rock, Paper, or Scissors
-    return prompt("What is your choice?: (Rock, Paper, Scissors").toLowerCase();
+    return String(prompt("What is your choice?: (Rock, Paper, Scissors").toLowerCase());
 }
 
 function playRound(playerSelection, computerSelection) {
