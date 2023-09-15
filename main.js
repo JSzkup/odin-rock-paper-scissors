6a8f2201b21c4274e46b669521318cb38047d9b2
Jonathan Szkup <jszkup101@gmail.com>
2023-09-14 20:26:31 -0400

    Add round playing functionality
    A single round can be played full between a player and the computer.

--- main.js
+++ main.js
@@ -21,9 +21,30 @@ function getPlayerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     // plays a SINGLE round of RPS, returns a string that declares the winner
+
+    let isWinner;
+
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        isWinner = True;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        isWinner = True;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        isWinner = True;
+    } else {
+        isWinner = False;
+    }
+
+    if (isWinner) {
+        return `You Win! - ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! - ${computerSelection} beats ${playerSelection}`;
+    }
+
 }
 
 function game() {
     // Plays 5 rounds in a loop keeping score, then prints the results into console
+
+    playRound(getPlayerChoice(), getComputerChoice());
 }
 
