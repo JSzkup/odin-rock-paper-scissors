adafde1f5e5880ccd89c0414430330bc7d13d67e
Jonathan Szkup <jszkup101@gmail.com>
2023-10-10 16:06:07 -0400

    Add default UI text, comments Game Win checking

--- scripts.js
+++ scripts.js
@@ -1,17 +1,19 @@
+// sets base score for each player
 let playerScore = 0;
 let compScore = 0;
 
+// creates and appends elements to the DOM to be updated
 const playerScoreContainer = document.querySelector('#player-score');
 const playerScoreContent = document.createElement('div');
 playerScoreContent.classList.add('playerScoreContent');
 playerScoreContainer.appendChild(playerScoreContent);
-
 
 const compScoreContainer = document.querySelector('#comp-score');
 const compScoreContent = document.createElement('div');
 compScoreContent.classList.add('compScoreContent');
 compScoreContainer.appendChild(compScoreContent);
 
+// default UI text
 playerScoreContent.textContent = "Player: "
 compScoreContent.textContent = "Computer: "
 
@@ -22,6 +24,7 @@ roundWinnerContent.classList.add('roundWinnerContent');
 
 roundWinnerContainer.appendChild(roundWinnerContent)
 
+// default UI text
 roundWinnerContent.textContent = "Click a button to play."
 
 function getComputerChoice() {
@@ -104,37 +107,16 @@ function playRound(playerSelection, computerSelection) {
 
     roundWinnerContainer.appendChild(roundWinnerContent);
 
-    // TODO run game() to check score for a winner
+    checkWinner();
 }
 
-function game() {
-    // Plays 5 rounds in a loop keeping score, then prints the results into console
+function checkWinner() {
+    if (playerScore >= 5) {
+        roundWinnerContent.textContent = "You won 5 rounds Congratulations!"
 
-    let winCount = 0;
-
-    // TODO if player-score or comp-score >= 5 conclude game, call out winner
-
-    // for (let round = 0; round < 5; round++) {
-    //     let currentRound = playRound(getPlayerChoice(), getComputerChoice());
-
-    //     console.log(currentRound);
-
-    //     if (currentRound.includes("Win!")) {
-    //         ++winCount;
-    //     } else if (currentRound.includes("Tie!")) {
-    //         --round;
-    //     }
-
-    // }
-
-    // const result = document.createElement('div');
-    // result.classList.add('result');
-
-    // if (5 - winCount <= 2) {
-    //     result.textContent = "You won the match!";
-    // } else {
-    //     result.textContent = "You lost the match.";
-    // }
+    } else if (compScore >= 5) {
+        roundWinnerContent.textContent = "The computer won 5 rounds, you lose."
+    }
 }
 
 window.addEventListener('load', getPlayerChoice);
